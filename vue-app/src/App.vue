6c3ff8e1eddc713551c005433f4a59ce0054a0cc
Victor Tolbert <victor.tolbert@gmail.com>
2021-06-20 19:08:39 -0400

    initialize vue-cli app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import HeaderBar from '@/components/header-bar.vue';
+import NavBar from '@/components/nav-bar.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+  components: {
+    HeaderBar,
+    NavBar,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <HeaderBar />
+    <div class="section columns">
+      <NavBar />
+      <main class="column">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
---
+++ vue-app/src/views/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="content-container">
+    <div class="content-title-group">
+      <h2 class="title">About Page</h2>
+    </div>
+  </div>
+</template>
